c575356f98379d309b27f1e7ccaa7fb901c6921f
Augustinegreg <augustineopoku02@gmail.com>
2023-08-21 06:39:19 -0700

    printf

--- 0x00-hello_world/5-printf.c
+++ 0x00-hello_world/5-printf.c
@@ -1,11 +1,11 @@
 #include <stdio.h>
 
 /*
- * main - prints main function
+ * main - main function
  * return - 0 if successful
  *
  */
-int main()
+int main(void)
 {
 	printf("with proper grammar, but the outcome is a piece of art,\n");
 
