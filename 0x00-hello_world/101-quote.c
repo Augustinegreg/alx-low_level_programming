6d9de0c94e6ce7b998ea51212e51fa190a876639
Augustinegreg <augustineopoku02@gmail.com>
2023-08-21 09:29:28 -0700

    continua

---
+++ 0x00-hello_world/101-quote.c
@@ -0,0 +1,14 @@
+#include <stdio.h>
+#include <unistd.h>
+
+/**
+ * main - writes to the stderr using
+ * description: the program writes to stderr.
+ * return: value 1 on success.
+ */
+int main(void)
+{
+	char *msg = "and that piece of art is useful\" - Dora Korpar, 2015-10-19\n";
+	write(2, msg, 59);
+	return 1;
+}
