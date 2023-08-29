dd7371695d508963f378f7179b2853ae64f2c9c6
Augustinegreg <augustineopoku02@gmail.com>
2023-08-29 04:55:35 -0700

    task 5

--- 0x07-pointers_arrays_strings/5-strstr.c
+++ 0x07-pointers_arrays_strings/5-strstr.c
@@ -13,7 +13,7 @@ char *_strstr(char *haystack, char *needle)
 {
 	int a, b;
 
-	for (a = 0; haystack[b] != '\0'; a++)
+	for (a = 0; haystack[a] != '\0'; a++)
 	{
 		for (b = 0; needle[b] != '\0'; b++)
 		{
