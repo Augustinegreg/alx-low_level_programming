4e18909fb02fc1a8564028d37a9268dee34252da
Augustinegreg <augustineopoku02@gmail.com>
2023-09-07 18:41:06 -0700

    advancedTask1

--- 0x0C-more_malloc_free/100-realloc.c
+++ 0x0C-more_malloc_free/100-realloc.c
@@ -13,7 +13,7 @@
 void *_realloc(void *ptr, unsigned int old_size, unsigned int new_size)
 {
 	char *ptr1;
-	char *old_ptr;
+	char *ptr2;
 	unsigned int i;
 
 	if (new_size == old_size)
@@ -32,18 +32,18 @@ void *_realloc(void *ptr, unsigned int old_size, unsigned int new_size)
 	if (!ptr1)
 		return (NULL);
 
-	old_ptr = ptr;
+	ptr2 = ptr;
 
 	if (new_size < old_size)
 	{
 		for (i = 0; i < new_size; i++)
-			ptr1[i] = old_ptr[i];
+			ptr1[i] = ptr2[i];
 	}
 
 	if (new_size > old_size)
 	{
 		for (i = 0; i < old_size; i++)
-			ptr1[i] = old_ptr[i];
+			ptr1[i] = ptr2[i];
 	}
 
 	free(ptr);
