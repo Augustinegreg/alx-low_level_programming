015f8a8988a05b431ba21e8c25044d347b9a2340
Augustinegreg <augustineopoku02@gmail.com>
2023-09-07 19:20:17 -0700

    AD1

--- 0x0C-more_malloc_free/100-realloc.c
+++ 0x0C-more_malloc_free/100-realloc.c
@@ -3,20 +3,16 @@
 
 /**
  * _realloc - reallocates a memory block using malloc and free
- *
  * @ptr: pointer to the memory previsouly allocated by malloc
- *
  * @old_size: size of the allocated memory for ptr
- *
  * @new_size: new size of the new memory block
- *
  * Return: pointer to the newly allocated memory block
  */
 void *_realloc(void *ptr, unsigned int old_size, unsigned int new_size)
 {
 	char *ptr1;
 	char *ptr2;
-	unsigned int i;
+	unsigned int q;
 
 	if (new_size == old_size)
 		return (ptr);
@@ -38,14 +34,14 @@ void *_realloc(void *ptr, unsigned int old_size, unsigned int new_size)
 
 	if (new_size < old_size)
 	{
-		for (i = 0; i < new_size; i++)
-			ptr1[i] = ptr2[i];
+		for (q = 0; q < new_size; q++)
+			ptr1[q] = ptr2[q];
 	}
 
 	if (new_size > old_size)
 	{
-		for (i = 0; i < old_size; i++)
-			ptr1[i] = ptr2[i];
+		for (q = 0; q < old_size; q++)
+			ptr1[q] = ptr2[q];
 	}
 
 	free(ptr);
