643a8e854403a9c557eae55057348a512706b70e
Augustinegreg <augustineopoku02@gmail.com>
2023-09-27 17:31:50 -0700

    Advanced Task

---
+++ 0x13-more_singly_linked_lists/101-print_listint_safe.c
@@ -0,0 +1,36 @@
+#include "lists.h"
+/**
+ * print_listint_safe - function that prints a linked list with a loop safely.
+ * @head: pointer to the 1st node of the linked list
+ * Return: new_node
+ */
+size_t print_listint_safe(const listint_t *head)
+{
+	const listint_t *tmpNode = NULL;
+	const listint_t *lnkdNode = NULL;
+	size_t a = 0;
+	size_t newNode;
+
+	tmpNode = head;
+	while (tmpNode)
+	{
+		printf("[%p] %d\n", (void *)tmpNode, tmpNode->n);
+		a = a + 1;
+		tmNode = tmpNode->next;
+		lnkdNode = head;
+		newNode = 0;
+		while (newNode < a)
+		{
+			if (tmpNode == lnkdNode)
+			{
+				printf("-> [%p] %d\n", (void *)tmNode, tmpNode->n);
+				return (a);
+			}
+			lnkd Node = lnkdNode->next;
+			newNode = newNode + 1;
+		}
+		if (!head)
+			exit(98);
+	}
+	return (a);
+}
